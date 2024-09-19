8607741f11039dbfd247482d24e4ce1815eff29d
delciojunior <delciomourainfo@gmail.com>
2024-09-18 22:00:17 -0300

    Sugestão de ajustes

---
+++ src/test/java/fixtures/RequestInformations.java
@@ -0,0 +1,9 @@
+package fixtures;
+
+import io.restassured.response.Response;
+
+public class RequestInformations {
+    public static final String url = "https://api.funtranslations.com/translate/";
+    public static String initializePhrase;
+    public static Response response;
+}
